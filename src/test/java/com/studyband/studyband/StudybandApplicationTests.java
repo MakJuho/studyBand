a1dd14496a4a8878297c3c8888cdaf14b92c1579
우주호 <einjuho7@ajou.ac.kr>
2020-11-19 11:14:59 +0900

    first commit

---
+++ src/test/java/com/studyband/studyband/StudybandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyband.studyband;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudybandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
